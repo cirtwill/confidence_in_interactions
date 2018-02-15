1b4a54c129e127fc63f3ac7f8b8b23b83f0150f7
Alyssa Cirtwill <alyci52@ifm16789.local>
2018-02-15 12:57:03 +0100

    attempting to undo merge errors:

---
+++ code/Zillis_prior/calculate_Ziller_simweb_properties.py
@@ -0,0 +1,134 @@
+import sys
+import os
+import networkx as nx 
+import numpy as np 
+import scipy as sp 
+
+# Think I will have to do NODF in R. vegan function nestednodf(web,order=TRUE,weighted=FALSE) should get it done
+# vegan takes community data as a matrix. This may be a problem. 
+# Something like the following should convert interaction list to matrix:
+# library(igraph);
+# edge_list <- read.delim("data_file.txt", header = TRUE, sep = "\t");
+# G <- graph.data.frame(edge_list,directed=FALSE);
+# A <- as_adjacency_matrix(G,type="both",names=TRUE,sparse=FALSE,attr="weight");
+
+def web_reader(webdir,proportion,post_number,sample_number,nettype,site): 
+  gallers=set()
+  salixes=set()
+  links=set()
+  if sample_number==0:
+    if nettype=="SG":
+      f=open(webdir+'posterior/P'+str(post_number)+'_'+site+'.web','r')
+    else:
+      f=open(webdir+'gp_posterior/P'+str(post_number)+'_'+site+'.web','r')
+  else:
+    if nettype=="SG":
+      f=open(webdir+'detection_filter/'+str(proportion)+'/P'+str(post_number)+'_'+site+'_'+str(sample_number)+'.web','r')
+    else:
+      f=open(webdir+'gp_detection_filter/'+str(proportion)+'/P'+str(post_number)+'_'+site+'_'+str(sample_number)+'.web','r')
+  for line in f:
+    galler=line.split()[0]
+    salix=line.split()[1]
+    gallers.add(galler)
+    salixes.add(salix)
+    links.add((galler,salix))
+  f.close()
+
+  # G=nx.Graph()
+  # for sp in gallers:
+  #   G.add_node(sp)
+  # for sp in salixes:
+  #   G.add_node(sp)
+  # for (galler,salix) in links:
+  #   G.add_edge(galler,salix)
+
+  return gallers,salixes,links
+
+def calculate_original_properties(gallers,salixes,links):
+  # original_properties['L'][post_number]=len(links)
+  # original_properties['S'][post_number]=len(G.nodes())
+  # original_properties['LS'][post_number]=float(len(G.edges()))/float(len(salixes))
+  # original_properties['LG'][post_number]=float(len(G.edges()))/float(len(gallers))
+  C=float(len(links))/float(len(salixes)*len(gallers))
+  LperSalix=[] # Ranges between 18 and 37 in the posterior predicted webs
+  LperGaller=[] # Ranges between 40 and 68
+  for salix in salixes:
+    slist=[]
+    for (g,s) in links:
+      if s==salix:
+        slist.append(g)
+    LperSalix.append(len(slist))
+  for galler in gallers:
+    glist=[]
+    for (g,s) in links:
+      if g==galler:
+        glist.append(s)
+    LperGaller.append(len(glist))
+
+  return C,np.mean(LperSalix),np.mean(LperGaller)
+
+def write_summary_file(summary_dict,nettype,site):
+  f=open('../../data/Zillis_webs/'+nettype+'_Connectance_table_'+site+'.tsv','w')
+  f.write('Web\tProportion\tObs_C\tSample_mean\tSample_SD\n')
+  for proportion in summary_dict['C']:
+    for post_number in summary_dict['C'][proportion]:
+      f.write(str(post_number)+'\t'+str(proportion)+'\t'+'\t'.join(summary_dict['C'][proportion][post_number])+'\n')
+  f.close()
+
+  g=open('../../data/Zillis_webs/'+nettype+'_LS_table_'+site+'.tsv','w')
+  g.write('Web\tProportion\tObs_LperSalix\tSample_mean\tSample_SD\n')
+  for proportion in summary_dict['LS']:
+    for post_number in summary_dict['LS'][proportion]:
+      g.write(str(post_number)+'\t'+str(proportion)+'\t'+'\t'.join(summary_dict['LS'][proportion][post_number])+'\n')
+  g.close()
+
+  h=open('../../data/Zillis_webs/'+nettype+'_LG_table_'+site+'.tsv','w')
+  h.write('Web\tProportion\tObs_LperGaller\tSample_mean\tSample_SD\n')
+  for proportion in summary_dict['LP']:
+    for post_number in summary_dict['LP'][proportion]:
+      h.write(str(post_number)+'\t'+str(proportion)+'\t'+'\t'.join(summary_dict['LP'][proportion][post_number])+'\n')
+  h.close()
+
+def main():
+
+  webdir='../../data/Zillis_webs/'
+
+  for site in ['Zillis','Zillertal']:
+    for nettype in ['SG','GP']:
+      print site, nettype
+      # Starting with C
+      summary_dict={'C':{0.5:{},0.6:{},0.7:{},0.8:{},0.9:{},0.95:{},0.99:{}},
+      'LS':{0.5:{},0.6:{},0.7:{},0.8:{},0.9:{},0.95:{},0.99:{}},
+      'LP':{0.5:{},0.6:{},0.7:{},0.8:{},0.9:{},0.95:{},0.99:{}}  }
+
+      for post_number in range(1,101):
+        gallers,salixes,links=web_reader(webdir,0,post_number,0,nettype,site)
+        obsC,obsLS,obsLP=calculate_original_properties(gallers,salixes,links)
+        for proportion in [0.5,0.6,0.7,0.8,0.9,0.95,0.99]:
+        # for proportion in [0.9,0.95,0.99]:
+          filtered_C=[]
+          filtered_LS=[]
+          filtered_LP=[]
+          for sample_number in range(1,101):
+            gallers,salixes,links=web_reader(webdir,proportion,post_number,sample_number,nettype,site)
+            C,LS,LP=calculate_original_properties(gallers,salixes,links)
+            filtered_C.append(C)
+            filtered_LS.append(LS)
+            filtered_LP.append(LP)
+          C_sample_mean=np.mean(filtered_C)
+          C_sample_SD=np.std(filtered_C)
+          LS_sample_mean=np.mean(filtered_LS)
+          LS_sample_SD=np.std(filtered_LS)
+          LP_sample_mean=np.mean(filtered_LP)
+          LP_sample_SD=np.std(filtered_LP)
+
+          summary_dict['C'][proportion][post_number]=(str(obsC),str(C_sample_mean),str(C_sample_SD))
+          summary_dict['LS'][proportion][post_number]=(str(obsLS),str(LS_sample_mean),str(LS_sample_SD))
+          summary_dict['LP'][proportion][post_number]=(str(obsLP),str(LP_sample_mean),str(LP_sample_SD))
+
+      print "Ready to write out the connectance file"
+      write_summary_file(summary_dict,nettype,site)
+
+  
+if __name__ == '__main__':
+  main()
---
+++ code/calculate_NODF.R
@@ -0,0 +1,48 @@
+library(vegan)
+library(igraph)
+
+for(webtype in c("SG","GP")){
+  if(webtype=="SG"){
+  path<-"../data/randomised_webs/posterior/"
+  propdir<-"../data/randomised_webs/detection_filter/"
+  } else {
+  path<-"../data/randomised_webs/gp_posterior/"
+  propdir<-"../data/randomised_webs/gp_detection_filter/"
+  }
+  print(webtype)
+  props<-c(0.5,0.6,0.7,0.8,0.9,0.95,0.99)
+  nesttable<-matrix(nrow=700,ncol=5)
+  colnames(nesttable)<-c("Web","Proportion","Obs_NODF","Sample_mean","Sample_SD")
+  for(i in 1:100){
+    print(i)
+    k<-1      
+  # Obs NODF only needs calculating once
+    webfile<-paste0(path,"P",i,".web",sep='')
+    intlist<-read.csv(webfile,sep='\t',header=FALSE)
+    G<-graph.data.frame(intlist,directed=TRUE)
+    web<-as_adjacency_matrix(G,type="both",names=TRUE,sparse=FALSE)
+    NODF<-nestednodf(web,order=TRUE,weighted=FALSE,wbinary=FALSE)$statistic["NODF"]
+    nesttable[k,1]<-paste0("P",i,".web",sep='')
+    nesttable[k,3]<-NODF
+    # Extract relevant filter webs
+    for(j in 1:7){
+      nesttable[k,2]<-props[j]
+      filterwebs<-as.character(list.files(path=paste0(propdir,props[j],sep=''),full.names=TRUE,pattern=paste0('P',i,'_',sep='')))
+      templist<-matrix(nrow=100,ncol=1)
+      # Calculate mean and SD for NODFs
+      for(q in 1:length(filterwebs)){
+        sampleints<-read.csv(filterwebs[q],sep='\t',header=FALSE)
+        newG<-graph.data.frame(sampleints,directed=TRUE)
+        newweb<-as_adjacency_matrix(newG,type="both",names=TRUE,sparse=FALSE)
+        newNODF<-nestednodf(newweb,order=TRUE,weighted=FALSE,wbinary=FALSE)$statistic["NODF"]
+        templist[q,1]<-newNODF
+      }
+    nesttable[k,4]<-mean(templist[,1])
+    nesttable[k,5]<-sd(templist[,1])
+  }
+
+    k<-k+1
+  }
+  write.table(nesttable,file=paste0('../data/randomised_webs/',webtype,'_NODF_table.tsv',sep=''))
+
+}
